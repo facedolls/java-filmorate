ff9515ea48afdca70496f0ea8a3f44d846c3ffcf
MuzykaNatalia <muzyka-natali@bk.ru>
2024-01-29 23:52:43 +0300

    fix: changed validation values classes UserController, User, Film, FilmController, create classes MinimumDateValidator, LoginValidator, create annotations AfterMinDateÐ, Login

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,17 +2,19 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.validator.Login;
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private int id;
+    private Integer id;
     @NotNull
     @NotBlank
     @Email
     private String email;
     @NotNull
     @NotBlank
+    @Login
     private String login;
     private String name;
     @NotNull
