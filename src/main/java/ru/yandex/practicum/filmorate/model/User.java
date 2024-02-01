1909549acc2d76499b46a3893832e1fce4d82afb
MuzykaNatalia <muzyka-natali@bk.ru>
2024-02-01 21:42:29 +0300

    fix: changed classes User, Film, FilmController, UserController, LoginValidator, interface AfterMinDate

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.validator.AfterMinDate;
 import ru.yandex.practicum.filmorate.validator.Login;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
@@ -12,18 +11,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     private Integer id;
-    @NotNull
     @NotBlank
     @Email
     private String email;
-    @NotNull
     @NotBlank
-    @Login
+    @Login(value = " ")
     private String login;
     private String name;
     @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
-    @AfterMinDate(value = "1924-01-01")
     @Past(message = "The date of birth cannot be the future")
     private LocalDate birthday;
 }
--- src/main/java/ru/yandex/practicum/filmorate/validator/AfterMinDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/AfterMinDate.java
@@ -10,5 +10,5 @@ public @interface AfterMinDate {
     String message() default "Date must not be before {value}";
     Class<?>[] groups() default {};
     Class<?>[] payload() default {};
-    String value() default "1895-12-28";
+    String value() default "";
 }
