d82424ac28d4ea43599b372a39c312dd563ce21d
MuzykaNatalia <143342134+MuzykaNatalia@users.noreply.github.com>
2024-04-04 20:57:50 +0300

    Добавлен вывод самых популярных фильмов по жанру и годам (#10)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -31,8 +31,10 @@ public class FilmController {
     }
 
     @GetMapping("/popular")
-    public Collection<Film> getPopularFilm(@RequestParam(defaultValue = "10") @Positive Integer count) {
-        return filmService.getPopularFilm(count);
+    public Collection<Film> getPopularFilm(@RequestParam(defaultValue = "10") @Positive Integer count,
+                                           @RequestParam(defaultValue = "0", required = false) Integer genreId,
+                                           @RequestParam(defaultValue = "0", required = false) Integer year) {
+        return filmService.getPopularFilm(count, genreId, year);
     }
 
     @GetMapping("/director/{directorId}")
